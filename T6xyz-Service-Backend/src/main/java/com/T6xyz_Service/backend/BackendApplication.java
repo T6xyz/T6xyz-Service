9d2afe59f82ebad64b51b578f891f2bc5d5b8f43
T6xyz <benjaminzwolenik@gmail.com>
2025-07-04 01:09:45 -0500

    Added React, Springboot, and AWS CDK packages

---
+++ T6xyz-Service-Backend/src/main/java/com/T6xyz_Service/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.T6xyz_Service.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ T6xyz-Service-Backend/src/test/java/com/T6xyz_Service/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T6xyz_Service.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
