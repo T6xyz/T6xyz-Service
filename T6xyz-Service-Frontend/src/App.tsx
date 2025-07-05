fef3d6e6c4bebfb6049a541fdacfa0738372345a
T6xyz <benjaminzwolenik@gmail.com>
2025-07-05 12:21:11 -0500

    add loader to test spinner

--- T6xyz-Service-Frontend/src/App.tsx
+++ T6xyz-Service-Frontend/src/App.tsx
@@ -1,8 +1,26 @@
 import './App.css'
 import { ShikiHighlighter } from 'react-shiki'
-import { Heading, Highlight, Stack, Text } from "@chakra-ui/react"
+import { Heading, Highlight, Spinner, Stack, Text } from "@chakra-ui/react"
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsPageLoaded(true);
+    };
+
+    if (document.readyState === 'complete') {
+      setIsPageLoaded(true);
+    } else {
+      window.addEventListener('load', handleLoad);
+    }
+
+    return () => window.removeEventListener('load', handleLoad);
+  }, []);
+
+
 
   const code = `
   package com.service.backend;
@@ -17,39 +35,46 @@ function App() {
       SpringApplication.run(BackendApplication.class, args);
     }
   }
-  `; // input code
+  `;
+
   
 
   return (
-    <div style={{marginBottom: "100px"}}>
-        <Stack>
-          <Heading size="3xl" letterSpacing="tight">
-            <Highlight query="with speed" styles={{ color: "teal.600" }}>
-              T6xyz.IO
-            </Highlight>
-          </Heading>
-          <Text fontSize="md" color="fg.muted">
-            This is sample code below for Spring Application
-          </Text>
-        </Stack>
-        <br></br>
-        <ShikiHighlighter
-        language="java"
-        className="code-block"
-        theme="github-dark-default"
-        showLanguage={true}
-        addDefaultStyles={true}
-        showLineNumbers={true}
-        startingLineNumber={1}
-        delay={150}
-        as="div"
-        style={{
-          textAlign: "left",
-          fontSize: "16px"
-        }}
-      >
-        {code}
-      </ShikiHighlighter>
+    <div>
+        {isPageLoaded ? (
+          <div style={{marginBottom: "100px"}}>
+            <Stack>
+              <Heading size="3xl" letterSpacing="tight">
+                <Highlight query="with speed" styles={{ color: "teal.600" }}>
+                  T6xyz.IO
+                </Highlight>
+              </Heading>
+              <Text fontSize="md" color="fg.muted">
+                This is sample code below for Spring Application
+              </Text>
+            </Stack>
+            <br></br>
+            <ShikiHighlighter
+              language="java"
+              className="code-block"
+              theme="github-dark-default"
+              showLanguage={true}
+              addDefaultStyles={true}
+              showLineNumbers={true}
+              startingLineNumber={1}
+              delay={150}
+              as="div"
+              style={{
+                textAlign: "left",
+                fontSize: "16px"
+              }}
+            >
+              {code}
+            </ShikiHighlighter>
+          </div>
+        ) : (
+          <Spinner size="lg" />
+        )}
     </div>
   )
 }
